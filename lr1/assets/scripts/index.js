5583000a2cff6e29f4f85bb4c4bb3d612b649bb4
Ari-Keisa <89538586+Ari-Keisa@users.noreply.github.com>
2025-06-22 23:36:14 +0300

    Add files via upload

---
+++ lr1/assets/scripts/index.js
@@ -0,0 +1,11 @@
+        const btnLinkedin = document.querySelector("#linkedin-button");
+
+        const btnWebsite = document.querySelector("#web-button");
+
+        btnLinkedin.addEventListener('click', () => {
+                window.open('https://youtu.be/9bhnBFKMWLA?si=mox6UDi2qYHA_nUL', '_blank');
+        });
+
+        btnWebsite.addEventListener('click', () => {
+                window.open('https://pin.it/13TMQeDKw', '_blank');
+        });
